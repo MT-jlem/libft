b08add31a46dc70a7120f3119ffe66034bff4c60
MT-jlem <jlemmustapha@gmail.com>
2022-07-04 16:37:18 +0100

    remove comments

--- ft_memset.c
+++ ft_memset.c
@@ -6,10 +6,11 @@
 /*   By: mjlem <mjlem@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 18:17:49 by mjlem             #+#    #+#             */
-/*   Updated: 2022/07/01 21:19:28 by mjlem            ###   ########.fr       */
+/*   Updated: 2022/07/02 21:58:55 by mjlem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
 {
