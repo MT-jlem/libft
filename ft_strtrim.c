5278301e81b71c38cbec445826694f88d8c2a819
Mustapha Jlem <mjlem@e2r8p5.1337.ma>
2022-11-28 16:18:15 +0100

    new changes

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,16 +12,57 @@
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+static int    checkbegin(char const *str, char const *c)
 {
-	size_t	end;
+    int    i;
 
-	if (!s1)
-		return (NULL);
-	while (*s1 && ft_strchr(set, *s1) != NULL)
-		s1++;
-	end = (ft_strlen(s1));
-	while (end && ft_strchr(set, s1[end]) != NULL)
-		end--;
-	return (ft_substr(s1, 0, end + 1));
+    i = 0;
+    while (i < ft_strlen(str))
+    {
+        if (ft_strchr(c, str[i]) == 0)
+            break ;
+        i++;
+    }
+    return (i);
+}
+
+static int    checkend(char const *str, char const *c)
+{
+    int    j;
+
+    j = ft_strlen(str) - 1;
+    while (str[j])
+    {
+        if (ft_strchr(c, str[j]) == 0)
+            break ;
+        j--;
+    }
+    return (j);
+}
+
+char    *ft_strtrim(char const *s1, char const *set)
+{
+    char    *temp;
+    int        j;
+    int        k;
+    int        i;
+
+    if (!s1)
+        return (NULL);
+    i = 0;
+    j = (checkend(s1, set) + 1);
+    k = checkbegin(s1, set);
+    if ((j - k) <= 0)
+        return (ft_strdup (""));
+    temp = (char *) malloc((j - k + 1) * sizeof (char));
+    if (!temp)
+        return (NULL);
+    while (k < j)
+    {
+        temp[i] = s1[k];
+        i++;
+        k++;
+    }
+    temp[i] = '\0';
+    return (temp);
 }
